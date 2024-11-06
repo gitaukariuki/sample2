6670f22627ae312a1bc8c42303cdbb14048a8e12
gitaukariuki <gitaukariuki@gmail.com>
2024-11-06 13:00:20 +0300

    Update data.js

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,30 +1,30 @@
-import p1_img from './product_1.png'
-import p2_img from './product_2.png'
-import p3_img from './product_3.png'
-import p4_img from './product_4.png'
+import p1_img from './01.jpg'
+import p2_img from './03.jpg'
+import p3_img from './05.jpg'
+import p4_img from './04.jpg'
 
 let data_product = [
   {
     id:1,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Detailed product description",
     image:p1_img,
     new_price:50.00,
     old_price:80.50,
   },
   {id:2,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Detailed product description",
     image:p2_img,
     new_price:85.00,
     old_price:120.50,
   },
   {id:3,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Detailed product description",
     image:p3_img,
     new_price:60.00,
     old_price:100.50,
   },
   {id:4,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name:"Detailed product description",
     image:p4_img,
     new_price:100.00,
     old_price:150.00,
